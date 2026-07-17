f363ef3c4802360661d2a6ea92d304da7415e17e
Akkiprakash <akkiprakash741@gmail.com>
2026-07-17 11:16:00 +0530

    problem resolved

---
+++ src/Innerstactic/static1.java
@@ -0,0 +1,17 @@
+class test1{
+    private String str = "Akki";
+    class test2{
+       void checking(){
+            System.out.println(str);
+        }
+    }
+}
+public class static1{
+    public static void main(String[] args) {
+        test1 test = new test1();
+        test1.test2 fin = test. new test2();
+        fin.checking();
+        
+    }
+
+}
